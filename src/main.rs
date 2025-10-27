b4581793b806ab7ea7ef7cafa8a2f6a539f572e1
Jack Toumey <jack@toumey.io>
2025-10-27 02:13:03 -0700

    first function implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// fn print_elements(elements: &Vec<String>) {}
+fn print_elements(elements: &[String]) {
+// for element in elements {
+  //   println!("{}", element);
+  // }
+  elements
+    .iter()
+    .map(|f| format!("{} {}", f, f))
+    .for_each(|f| println!("{}", f));
+}
+
 fn main() {
-  println!("Hello, world!");
+  let colors = vec![
+    String::from("red"),
+    String::from("blue"),
+    String::from("green"),
+  ];
+
+  // let mut colors_iter = colors.iter();
+
+  // println!("{:#?}", colors_iter.next());
+  // println!("{:#?}", colors_iter.next());
+  // println!("{:#?}", colors_iter.next());
+  // println!("{:#?}", colors_iter.next());
+
+  print_elements(&colors);
 }
